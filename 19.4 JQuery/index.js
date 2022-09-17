1b80fee21da9056c30807add58a92cce34caedd7
albertzhao <szdxzhy@outlook.com>
2022-09-17 10:18:42 +0800

    增加JQuery的使用

--- 19.4 JQuery/index.js
+++ 19.4 JQuery/index.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
-    $("span").click(function(){
-        $(this).hide();
+    // jQuery functions go here
+    // 开始动画
+    $(".OpenJQuery").click(function(){
+        $(".spanFirst").hide(10000,function(){
+            console.log("span 已经隐藏了");
+        });
+        $(".thirdSpan").slideToggle(10000,function(){
+            console.log("span 开始滑动");
+        });
+
+        $(".secondSpan").animate({
+            left:'250px',
+            opacity:'0.5',
+            height:'150px',
+            width:'150px'
+        },5000,function(){
+            console.log("span 开始执行动画了");
+        })
+    });
+
+    // 停止动画
+    $(".CloseJQuery").click(function(){
+        console.log("停止动画按钮被点击");
+        // 返回关闭 JQuery
+        console.log($(this).text());
+        console.log($(this).html());
+        $(".thirdSpan").stop();
+        $(".thirdSpan").append("我是插入的元素");
+        $(".thirdSpan").after("我是插入的元素");
     })
-})
+  });
